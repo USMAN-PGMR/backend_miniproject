f702a586f37e0b339c8ac4a427653a7cb544fc0e
M Usman ali <usmanpgmr@gmail.com>
2025-03-18 01:47:27 +0500

    setup public static files

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ const path =require('path');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set('view engine','ejs')   //Ejs
+//set public static files 
+app.use(express.static(path.join(__dirname,'public')))   // __direname overall path data ha system sy ap k folder tak
+// console.log(__dirname)
 
 
 
 // ********routes******
+// app.get("/",function(req,res){
+//     res.send('working fine ')
+// })
+// oper normal routes thy jo brower pr show hoty thy lkn ab ejs waly routes 
+// set ejs 
 app.get("/",function(req,res){
-    res.send('working fine ')
+    res.render('index')     // is me ham render karain gy or views folder k pages ko use karain gy 
 })
 
 
