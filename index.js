990efff0a9933d8260e04078ff0a39e638ab9764
M Usman ali <usmanpgmr@gmail.com>
2025-03-18 23:35:50 +0500

    notes create and show

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express();
 const path =require('path');
+const fs=require('fs');
 
 // ----middle ware ------parser/cookie 
 app.use(express.json());
@@ -20,8 +21,23 @@ app.use(express.static(path.join(__dirname,'public')))   // __direname overall p
 // oper normal routes thy jo brower pr show hoty thy lkn ab ejs waly routes 
 // -----------set ejs 
 app.get("/",function(req,res){
-    res.render('index')     // is me ham render karain gy or views folder k pages ko use karain gy 
+    fs.readdir('./files',function(err,files){
+        // res.render('index')     // is me ham render karain gy or views folder k pages ko use karain gy 
+        res.render('index',{files:files})      
+
+    })
 })
+// post methos 
+app.post("/create", function (req, res) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.description, function (err) {
+        if (err) {
+            console.error("Error saving the file:", err);
+            return res.status(500).send("Error saving the file");
+        }
+        console.log("File saved successfully");
+        res.redirect("/");
+    });
+});
 // *********dynamic routes *********
 app.get("/profile/:username",function(req,res){
     //(req.params.username) // ya params dynamically part k data ko dakhy ga 
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,3 @@
-alert(
-    'welcom'
-);
+// alert(
+//     'welcom'
+// );
